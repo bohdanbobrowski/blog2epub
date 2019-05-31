86f391b7e4ded5376da5e91d7b50886ccb7f2b3c
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2019-05-31 16:00:16 +0200

    Refactoring

--- blog2epub/Blog2Epub.py
+++ blog2epub/Blog2Epub.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding : utf-8 -*-
 
+from blog2epub.crawlers.Crawler import Crawler
+from blog2epub.crawlers.CrawlerBlogspot import CrawlerBlogspot
+
 class Blog2Epub(object):
     """
     Main Blog2Epub class.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, url, params={}):
+        self.url = url
+        self.crawler = self._selectCrawler(url, params)
 
-    def main(self):
-        pass
+    def _selectCrawler(self, url, params):
+        self.crawler = Crawler(params)
+        self.crawler = CrawlerBlogspot(params)
+
+    def download(self):
+        self.crawler.crawl()
+        self.crawler.save()
+
