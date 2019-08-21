61cf414f2a3538d36b853f62bc114f102d18b476
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2019-08-21 13:41:18 +0200

    ttk - themed tkinter

--- blog2epub/Blog2EpubGui.py
+++ blog2epub/Blog2EpubGui.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding : utf-8 -*-
-import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
 import sys
 import os
+import platform
+# if platform.system() == 'Darwin':
 from pathlib import Path
 
 from blog2epub.Blog2Epub import Blog2Epub
 from blog2epub.crawlers.Crawler import EmptyInterface
 from urllib import parse
+
 
 class TkInterface(EmptyInterface):
 
@@ -16,28 +20,28 @@ class TkInterface(EmptyInterface):
         self.refresh = refresh
 
     def print(self, text):
-        self.consoleOutput.insert(tk.END, text + '\n')
+        self.consoleOutput.insert(END, text + '\n')
         self.consoleOutput.see('end')
         self.refresh()
 
     def exception(self, e):
-        self.consoleOutput.insert(tk.END, e + '\n')
+        self.consoleOutput.insert(END, e + '\n')
         self.consoleOutput.see('end')
         self.refresh()
 
     def clear(self):
-        self.consoleOutput.delete(1.0, tk.END)
+        self.consoleOutput.delete(1.0, END)
 
 
-class Blog2EpubGui(tk.Frame):
+class Blog2EpubGui(Frame):
 
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)                 
+        Frame.__init__(self, master)                 
         self.master = master
-        self.consoleOutput = tk.Text(self.master)
-        self.urlEntry = tk.Entry(self.master, width=10)
-        self.limitEntry = tk.Entry(self.master, width=10)
-        self.skipEntry = tk.Entry(self.master, width=10)
+        self.consoleOutput = Text(self.master)
+        self.urlEntry = Entry(self.master, width=10)
+        self.limitEntry = Entry(self.master, width=10)
+        self.skipEntry = Entry(self.master, width=10)
         self.interface = TkInterface(self.consoleOutput, self.master.update)
         self.init_window()
 
@@ -62,16 +66,16 @@ class Blog2EpubGui(tk.Frame):
     def init_window(self):
         self.master.title("Blog2Epub")
         # Url:
-        tk.Label(self.master, text="Url:").grid(row=0)
-        self.urlEntry.grid(row=0, column=1, columnspan=2, sticky=tk.W+tk.E)
+        Label(self.master, text="Url:").grid(row=0)
+        self.urlEntry.grid(row=0, column=1, columnspan=2, sticky=W+E)
         # Button:
-        downloadButton = tk.Button(self.master, text="Download", command=self.download)
+        downloadButton = Button(self.master, text="Download", command=self.download)
         downloadButton.grid(row=0, column=3)
         # Limit:
-        tk.Label(self.master, text="Limit:").grid(row=1)
+        Label(self.master, text="Limit:").grid(row=1)
         self.limitEntry.grid(row=1, column=1)
         # Skip:
-        tk.Label(self.master, text="Skip:").grid(row=1, column=2)
+        Label(self.master, text="Skip:").grid(row=1, column=2)
         self.skipEntry.grid(row=1, column=3)
         # Text
         self.consoleOutput.grid(row=2, columnspan=4)
@@ -93,8 +97,7 @@ class Blog2EpubGui(tk.Frame):
 
 
 def main():
-    root = tk.Tk()
+    root = Tk()
     root.resizable(False, False)
-    # root.geometry("600x600")
-    app = Blog2EpubGui(root)
+    Blog2EpubGui(root)
     root.mainloop() 
