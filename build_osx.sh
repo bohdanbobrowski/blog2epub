a0c0508a5da1e5c12c1eb92882f9b2f04b27d4e8
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2021-10-14 19:41:44 +0200

    Kivy UI

--- build_osx.sh
+++ build_osx.sh
@@ -3,5 +3,5 @@
 rm -rf ./build
 rm -rf ./dist
 pyinstaller --onefile --windowed blog2epubgui_osx.spec
-cp -v ./blog2epub/assets/*.ttf ./dist/blog2epub.app/Contents/MacOS
-cp -v ./images/blog2epub.png ./dist/blog2epub.app/Contents/MacOS
+cp -v ./blog2epub/assets/*.ttf ./dist/blog2epubgui.app/Contents/MacOS
+cp -v ./images/blog2epub.png ./dist/blog2epubgui.app/Contents/MacOS
