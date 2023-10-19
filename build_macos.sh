af002fdb818257567076c92b04325549a564e5e5
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2023-10-19 18:42:38 +0200

    Improving macos build - dmg now contains Applications folder shortcut to ease installation

--- build_macos.sh
+++ build_macos.sh
@@ -2,13 +2,16 @@
 
 rm -rf ./build
 rm -rf ./dist
-pyinstaller blog2epub_osx.spec
+pyinstaller blog2epub_macos.spec
 if [ -d "./dist/blog2epub.app" ]; then
   cp -v ./blog2epub/assets/*.ttf ./dist/blog2epub.app/Contents/Resources
   cp -v ./images/blog2epub.png ./dist/blog2epub.app/Contents/Resources
   cp -v ./images/blog2epub.icns ./dist/blog2epub.app/Contents/Resources
   mkdir -p ./dist/osx/
   mv ./dist/blog2epub.app ./dist/osx/
+  ln -s /Applications ./dist/osx/Applications
+  # ln -s ~/Applications ./dist/osx/Applications
+  # mv ./dist/osx/Applications "./dist/osx/ "
   hdiutil create /tmp/tmp.dmg -ov -volname "blog2epub" -fs HFS+ -srcfolder "./dist/osx/"
   hdiutil convert /tmp/tmp.dmg -format UDZO -o ./dist/blog2epub.dmg
 fi
