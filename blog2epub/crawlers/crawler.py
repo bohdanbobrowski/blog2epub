dde3a334a55958142a2e252b8258caac46519703
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-02-29 16:01:28 +0100

    dateutil.parser.ParseError fix

--- blog2epub/crawlers/crawler.py
+++ blog2epub/crawlers/crawler.py
@@ -16,7 +16,6 @@ from typing import Optional
 
 import atoma
 import dateutil.parser
-import dateutil.ParserError
 import requests
 from lxml.ElementInclude import etree
 from lxml.html.soupparser import fromstring
@@ -462,7 +461,7 @@ class Article:
             self.date = self._translate_month(self.date)
         try:
             self.date = dateutil.parser.parse(self.date)
-        except dateutil.ParserError:
+        except dateutil.parser.ParserError:
             self.interface.print(f"Date not parsed: {self.date}")
 
     def _translate_month(self, date: str) -> str:
