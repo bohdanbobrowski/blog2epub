0129f4a4fb410ec53316b797c175c8b1623e9145
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-03-14 10:27:16 +0100

    Models skeleton and reformat

--- blog2epub/models/book.py
+++ blog2epub/models/book.py
@@ -1,17 +1,34 @@
+from datetime import datetime
 from typing import List, Optional
 
 from pydantic import BaseModel, HttpUrl
 
 
-class BookSynopsis(BaseModel):
+class BookSynopsisModel(BaseModel):
     title: Optional[str]
     subtitle: Optional[str]
     urls: List[HttpUrl]
 
 
+class CommentModel(BaseModel):
+    title: Optional[str]
+    date: Optional[datetime]
+    author: Optional[str]
+    content: Optional[str]
+
+
 class ArticleModel(BaseModel):
-    pass
+    url: HttpUrl
+    title: Optional[str]
+    subtitle: Optional[str]
+    date: Optional[datetime]
+    content: Optional[str]
+    comments: List[CommentModel]
 
 
 class BookModel(BaseModel):
-    pass
+    url: HttpUrl
+    title: Optional[str]
+    subtitle: Optional[str]
+    articles: List[ArticleModel]
+
