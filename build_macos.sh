82379bdbedc65ea5b5317ca17167cf6bd865a201
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-07-20 13:47:02 +0200

    macOS build preparation

--- build_macos.sh
+++ build_macos.sh
@@ -9,7 +9,7 @@ if [ -d "./dist/blog2epub.app" ]; then
   cp -v ./assets/MartianMono-Regular.ttf ./dist/blog2epub.app/Contents/Resources
   cp -v ./assets/blog2epub.png ./dist/blog2epub.app/Contents/Resources
   mkdir -p ./dist/macos_dng_image/
-  mv ./dist/blog2epub.app ./dist/macos_dng_image/
+  cp ./dist/blog2epub.app ./dist/macos_dng_image/
   ln -s /Applications ./dist/macos_dng_image/Applications
   # ln -s ~/Applications ./dist/macos_dng_image/Applications
   # mv ./dist/macos_dng_image/Applications "./dist/macos_dng_image/ "
