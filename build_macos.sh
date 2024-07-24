41a1c24b2a7fa26a270dc280a5b5019b9bea7070
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-07-24 22:38:26 +0200

    Fixing macos builds

--- build_macos.sh
+++ build_macos.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 if [ -d "./dist/blog2epub.app" ]; then
-  if [ -d "./dist/macos_dng_image" ]; then
-    rm -r "./dist/macos_dng_image"
-  fi
-  if [ -f "./dist/blog2epub.dmg" ]; then
-    rm "./dist/blog2epub.dmg"
-  fi
-  # TODO: investigate why these 4 files were not copied to app
-  cp -v ./assets/Alegreya-Regular.ttf ./dist/blog2epub.app/Contents/Resources
-  cp -v ./assets/Alegreya-Italic.ttf ./dist/blog2epub.app/Contents/Resources
-  cp -v ./assets/MartianMono-Regular.ttf ./dist/blog2epub.app/Contents/Resources
-  cp -v ./assets/blog2epub.png ./dist/blog2epub.app/Contents/Resources
   mkdir -p ./dist/macos_dng_image/
   cp -r ./dist/blog2epub.app ./dist/macos_dng_image/
   ln -s /Applications ./dist/macos_dng_image/Applications
