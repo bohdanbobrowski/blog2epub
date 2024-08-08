ebe575737251bd273535ab640534cd7eacf0c7d7
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-08-08 12:59:55 +0200

    Tests skeleton

---
+++ tests/unit/blog2epub/crawlers/test_universal.py
@@ -0,0 +1,33 @@
+import unittest
+
+# import pytest
+from blog2epub.crawlers import UniversalCrawler
+
+
+class TestUniversalCrawler(unittest.TestCase):
+    def setUp(self):
+        self.crawler = UniversalCrawler()
+
+    def test_robots(self):
+        # Given
+        with open("tests/unit/blog2epub/crawlers/data/robots-1.txt") as f:
+            given_robots = f.read()
+        # When
+        # Then
+        print(given_robots)
+
+    def test_sitemap_1(self):
+        # Given
+        with open("tests/unit/blog2epub/crawlers/data/sitemap-1.xml") as f:
+            given_sitemap = f.read()
+        # When
+        # Then
+        print(given_sitemap)
+
+    def test_sitemap_2(self):
+        # Given
+        with open("tests/unit/blog2epub/crawlers/data/sitemap-2.xml") as f:
+            given_sitemap = f.read()
+        # When
+        # Then
+        print(given_sitemap)
