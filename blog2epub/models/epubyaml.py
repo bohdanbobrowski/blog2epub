0a7b398cbca35ea47c731898ef0e32251f2ed252
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-08-24 09:27:58 +0200

    EpubYaml - remove debug

--- blog2epub/models/epubyaml.py
+++ blog2epub/models/epubyaml.py
@@ -1,6 +1,5 @@
 from typing import List, Optional
 from pydantic import BaseModel
-from pydantic_yaml import to_yaml_str
 
 
 class EpubYamlChapter(BaseModel):
@@ -12,20 +11,4 @@ class EpubYamlChapter(BaseModel):
 class EpubYamlModel(BaseModel):
     title: str
     subtitle: str
-    cover: Optional[str] = None
     chapters: List[EpubYamlChapter] = []
-
-
-if __name__ == "__main__":
-    bf = EpubYamlModel(
-        title="test ebook",
-        subtitle="test ebook",
-    )
-    for x in range(0, 5):
-        bf.chapters.append(
-            EpubYamlChapter(
-                title=f"test chapter {x}",
-                url=f"http://example.com/chapter_{x}.html",
-            )
-        )
-    print(to_yaml_str(bf))
