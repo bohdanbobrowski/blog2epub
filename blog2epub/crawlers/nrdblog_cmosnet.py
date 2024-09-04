917e5ac144a8269e9d847a8fa3f99a873e88ef7f
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-09-04 12:18:30 +0200

    Two blank crawlers to be developed in near future

---
+++ blog2epub/crawlers/nrdblog_cmosnet.py
@@ -0,0 +1,11 @@
+from blog2epub.crawlers import AbstractCrawler
+
+
+class NrdblogCmosEuCrawler(AbstractCrawler):
+    """TODO: https://nrdblog.cmosnet.eu"""
+
+    def crawl(self):
+        pass
+
+    def generate_ebook(self, **kwargs):
+        pass
---
+++ blog2epub/crawlers/zeissikonveb.py
@@ -0,0 +1,11 @@
+from blog2epub.crawlers import AbstractCrawler
+
+
+class ZeissIkonVEBCrawler(AbstractCrawler):
+    """TODO: https://zeissikonveb.de"""
+
+    def crawl(self):
+        pass
+
+    def generate_ebook(self, **kwargs):
+        pass
