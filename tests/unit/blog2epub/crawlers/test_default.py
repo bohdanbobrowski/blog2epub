b9d2ad9283cb94941047f185e7331ec4d10707a7
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-11-14 15:37:46 +0100

    Unit testing sitemaps parsing

--- tests/unit/blog2epub/crawlers/test_default.py
+++ tests/unit/blog2epub/crawlers/test_default.py
@@ -1,9 +1,10 @@
 import tempfile
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 from blog2epub.common.interfaces import EmptyInterface
-from blog2epub.crawlers import DefaultCrawler
+from blog2epub.crawlers.default import DefaultCrawler
 from blog2epub.models.configuration import ConfigurationModel
 
 
@@ -11,26 +12,57 @@ from blog2epub.models.configuration import ConfigurationModel
 def mock_configuration() -> ConfigurationModel:
     return ConfigurationModel(
         destination_folder=tempfile.gettempdir(),
-        limit="1",
+        limit="2",
     )
 
 
-class TestDefaultCrawler:
-    # TODO: change this tests to unit test
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, file_name, status_code=200):
+            self.file_name = file_name
+            self.status_code = status_code
 
+        @property
+        def content(self):
+            if self.file_name:
+                with open(f"tests/unit/blog2epub/crawlers/data/{self.file_name}", "rb") as f:
+                    content = f.read()
+                return content
+            return None
+
+    if args and len(args) > 0:
+        fname = args[0].replace("http://", "").replace("https://", "").replace("/", "_")
+        if fname.find(".xml?page=") != -1:
+            fname = fname.replace(".xml?page=", "") + ".xml"
+        return MockResponse(fname, 200)
+
+    return MockResponse(None, 404)
+
+
+class TestDefaultCrawler:
+    @patch(
+        "urllib.robotparser.RobotFileParser",
+        MagicMock(return_value=MagicMock(sitemaps=["https://bohdan.bobrowski.com.pl/wp-sitemap.xml"])),
+    )
     def test_get_sitemap_url(self, mock_configuration):
-        # given
+        # Given
+        with open("tests/unit/blog2epub/crawlers/data/robots-1.txt") as f:
+            f.read()
         given_crawler = DefaultCrawler(
             url="bohdan.bobrowski.com.pl",
-            interface=EmptyInterface(),
             configuration=mock_configuration,
-            cache_folder="tests_cache",
+            interface=EmptyInterface(),
         )
-        # when
-        sitemap_url = given_crawler._get_sitemap_url()
-        # then
-        assert sitemap_url == "https://bohdan.bobrowski.com.pl/wp-sitemap.xml"
+        # When
+        result = given_crawler._get_sitemap_url()
+        # Then
+        assert result == "https://bohdan.bobrowski.com.pl/wp-sitemap.xml"
 
+    @patch(
+        "urllib.robotparser.RobotFileParser",
+        MagicMock(return_value=MagicMock(sitemaps=["https://bohdan.bobrowski.com.pl/wp-sitemap.xml"])),
+    )
+    @patch("requests.get", MagicMock(side_effect=mocked_requests_get))
     def test_get_pages_urls(self, mock_configuration):
         # given
         given_crawler = DefaultCrawler(
@@ -50,6 +82,11 @@ class TestDefaultCrawler:
         assert "https://bohdan.bobrowski.com.pl/wp-sitemap-taxonomies-post_tag-1.xml" not in pages
         assert "https://bohdan.bobrowski.com.pl/wp-sitemap-users-1.xml" not in pages
 
+    @patch(
+        "urllib.robotparser.RobotFileParser",
+        MagicMock(return_value=MagicMock(sitemaps=["https://rocket-garage.blogspot.com/sitemap.xml"])),
+    )
+    @patch("requests.get", MagicMock(side_effect=mocked_requests_get))
     def test_rocket_garage_blogspot_com(self, mock_configuration):
         # given
         given_crawler = DefaultCrawler(
