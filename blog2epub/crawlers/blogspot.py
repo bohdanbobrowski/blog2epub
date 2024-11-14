c0dd333ea7dd072d879207f7e60433eaa89b6454
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2024-11-14 16:51:26 +0100

    boston1775.blogspot.com fixes

--- blog2epub/crawlers/blogspot.py
+++ blog2epub/crawlers/blogspot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding : utf-8 -*-
 from blog2epub.crawlers.default import DefaultCrawler
+from blog2epub.models.content_patterns import Pattern
 
 
 class BlogspotCrawler(DefaultCrawler):
@@ -9,11 +10,23 @@ class BlogspotCrawler(DefaultCrawler):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.name = "blogger.com crawler"
-        self.content_xpath = '//div[contains(@itemprop, "articleBody")]'
-        self.images_regex = r'<table[^>]*><tbody>[\s]*<tr><td[^>]*><a href="([^"]*)"[^>]*><img[^>]*></a></td></tr>[\s]*<tr><td class="tr-caption" style="[^"]*">([^<]*)'
-        self.articles_regex = (
-            r"<h3 class=\'post-title entry-title\' itemprop=\'name\'>[\s]*<a href=\'([^\']*)\'>([^>^<]*)</a>[\s]*</h3>"
+
+        self.patterns.content.append(
+            Pattern(
+                xpath="//div[contains(@class, 'post-body')]",
+            )
         )
+        self.patterns.images.append(
+            Pattern(
+                xpath="//div[contains(@class, 'post-body')]//img",
+            )
+        )
+        self.patterns.content.append(
+            Pattern(
+                xpath='//div[contains(@itemprop, "articleBody")]',
+            )
+        )
+
         self.ignore_downloads += [
             r"https:\/\/www.blogger.com\/img\/blogger_logo_[a-z]+_[0-9]+\.png",
             r"https:\/\/resources.blogblog.com\/img\/[a-z0-9_]+.gif",
--- blog2epub/models/configuration.py
+++ blog2epub/models/configuration.py
@@ -1,6 +1,22 @@
+import random
 from pathlib import Path
 
 from pydantic import BaseModel
+
+example_blogs = [
+    "http://historicaltidbits.blogspot.com/",
+    "http://starybezpiek.blogspot.com/",
+    "https://19thcentury.wordpress.com/",
+    "https://knippsen.blogspot.com",
+    "https://ksgedania.blogspot.com",
+    "https://rocket-garage.blogspot.com",
+    "https://velosov.blogspot.com",
+    "https://www.blog.homebrewing.pl",
+    "https://www.historyoftheancientworld.com/",
+    "https://www.mikeanderson.biz/",
+    "https://www.nomadicmatt.com/",
+    "https://www.szarmant.pl/",
+]
 
 
 class ConfigurationModel(BaseModel):
@@ -10,7 +26,7 @@ class ConfigurationModel(BaseModel):
     images_size: list[int] = [600, 800]
     images_quality: int = 40
     url: str = ""
-    limit: str = ""
+    limit: str = "5"
     skip: str = ""
     history: list[str] = []
     email: str = ""
@@ -18,3 +34,5 @@ class ConfigurationModel(BaseModel):
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
+        if self.url == "":
+            self.url = random.choice(example_blogs)
