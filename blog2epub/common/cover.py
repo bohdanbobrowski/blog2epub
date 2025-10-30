d1a94de05af8c5abf46c7287a1f11d7490e6e33e
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2025-10-30 19:23:00 +0100

    Fixing android build, and better precommit

--- blog2epub/common/cover.py
+++ blog2epub/common/cover.py
@@ -69,9 +69,9 @@ class Cover:
             elif img.size[0] > img.size[1]:
                 new_x = img.size[0] / img.size[1] * self.tile_size
                 new_y = self.tile_size
-            img = img.resize((new_x, new_y), Image.LANCZOS)
+            img = img.resize((new_x, new_y), Image.LANCZOS)  # type: ignore
         cropped_img = self._crop_image(img)
-        cropped_img.thumbnail(size, Image.LANCZOS)
+        cropped_img.thumbnail(size, Image.LANCZOS)  # type: ignore
         return cropped_img
 
     def _is_landscape(self, width, height):
