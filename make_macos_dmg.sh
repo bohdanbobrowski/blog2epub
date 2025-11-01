0ea716641bdb04b2798851f8ac8b040cf0416599
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2025-11-01 17:43:20 +0100

    Mac build

--- make_macos_dmg.sh
+++ make_macos_dmg.sh
@@ -4,5 +4,5 @@ if [ -d "./dist/blog2epub.app" ]; then
   cp -r ./dist/blog2epub.app ./dist/macos_dng_image/
   ln -s /Applications ./dist/macos_dng_image/Applications
   hdiutil create /tmp/tmp.dmg -ov -volname "blog2epub" -fs HFS+ -srcfolder "./dist/macos_dng_image/"
-  hdiutil convert /tmp/tmp.dmg -format UDZO -o ./dist/blog2epub_v1.3.0_macos.dmg
+  hdiutil convert /tmp/tmp.dmg -format UDZO -o ./dist/blog2epub_v1.5.0_macos.dmg
 fi
